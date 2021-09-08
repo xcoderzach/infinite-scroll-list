d4d4e4f2973fe11cdb23e3aaca7a477713ed1dfc
Zach Smith <x.coder.zach@gmail.com>
2021-09-08 13:29:33 -0400

    initial nextjs app

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Main() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Home Listings</title>
+      </Head>
+      <main>
+      </main>
+    </div>
+  )
+}
